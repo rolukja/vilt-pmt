f1177a44905b4d2a8e91fc00dbfb9fa4b569d50c
ROvkia <romanlukjanow@gmail.com>
2025-01-28 20:35:23 +0100

    base admin controller

---
+++ app/Http/Controllers/Admin/AdminOverviewController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminOverviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        echo 'gg';
+    }
+
+}
