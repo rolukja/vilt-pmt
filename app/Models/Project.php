248370c7d7720e3c58765c76b599f7cc92cbd153
ROvkia <romanlukjanow@gmail.com>
2025-02-10 21:20:04 +0100

    set DocBlock Models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,26 @@ namespace App\Models;
 
 use App\Models\Base\Project as BaseProject;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Task> $tasks
+ * @property-read int|null $tasks_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Project newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Project newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Project query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Project whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Project extends BaseProject
 {
 	protected $fillable = [
--- app/Models/ProjectTask.php
+++ app/Models/ProjectTask.php
@@ -4,6 +4,26 @@ namespace App\Models;
 
 use App\Models\Base\ProjectTask as BaseProjectTask;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $project_id
+ * @property int $task_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Project $project
+ * @property-read \App\Models\Task $task
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ProjectTask newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ProjectTask newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ProjectTask query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ProjectTask whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ProjectTask whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ProjectTask whereProjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ProjectTask whereTaskId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|ProjectTask whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class ProjectTask extends BaseProjectTask
 {
 	protected $fillable = [
