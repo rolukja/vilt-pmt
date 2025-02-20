3206eef518bc52d3a15b216560e01b760b33d356
ROvkia <romanlukjanow@gmail.com>
2025-02-21 00:20:18 +0100

    add conditional rendering for Admin link based on user authentication

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Auth;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tighten\Ziggy\Ziggy;
@@ -30,12 +31,16 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        return [
-            ...parent::share($request),
-            'ziggy' => fn () => [
-                ...(new Ziggy)->toArray(),
-                'location' => $request->url(),
+        return array_merge(parent::share($request), [
+            'auth' => [
+                'user' => Auth::user(),
+                'isAdmin' => optional(Auth::user())->hasRole('admin'),
             ],
-        ];
+            'ziggy' => function () use ($request) {
+                return array_merge((new Ziggy())->toArray(), [
+                    'location' => $request->url(),
+                ]);
+            },
+        ]);
     }
 }
