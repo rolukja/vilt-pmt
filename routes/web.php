dd89e80bd1d7162ae7817094d835feaf31c133d5
ROvkia <romanlukjanow@gmail.com>
2025-02-23 11:25:38 +0100

    add user resource routes for UserController

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,14 @@ Route::name('task.')->group(function () {
     Route::delete('/task/{task}', [App\Http\Controllers\TaskController::class, 'destroy'])->name('destroy');
     Route::post('/task/{task}/restore', [App\Http\Controllers\TaskController::class, 'restore'])->name('restore');
 });
+Route::resource('project', \App\Http\Controllers\ProjectController::class);
+Route::name('user.')->group(function () {
+    Route::get('/user', [App\Http\Controllers\UserController::class, 'index'])->name('index');
+    Route::get('/user/create', [App\Http\Controllers\UserController::class, 'create'])->name('create');
+    Route::post('/user', [App\Http\Controllers\UserController::class, 'store'])->name('store');
+    Route::get('/user/{user}', [App\Http\Controllers\UserController::class, 'show'])->name('show');
+    Route::get('/user/{user}/edit', [App\Http\Controllers\UserController::class, 'edit'])->name('edit');
+    Route::put('/user/{user}', [App\Http\Controllers\UserController::class, 'update'])->name('update');
+    Route::delete('/user/{user}', [App\Http\Controllers\UserController::class, 'destroy'])->name('destroy');
+    Route::post('/user/{user}/restore', [App\Http\Controllers\UserController::class, 'restore'])->name('restore');
+});
