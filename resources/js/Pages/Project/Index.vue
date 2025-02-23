d715091ee0f4d37c7d58a0f872f038f38ad88a23
ROvkia <romanlukjanow@gmail.com>
2025-02-23 11:25:51 +0100

    add project list with create, edit, and delete functionality

--- resources/js/Pages/Project/Index.vue
+++ resources/js/Pages/Project/Index.vue
@@ -1,36 +1,53 @@
 <script setup>
-
-import AppLayout from "@/Layouts/AppLayout.vue";
-
+import { router } from '@inertiajs/vue3';
+import AppLayout from '@/Layouts/AppLayout.vue';
 
 const props = defineProps({
-  projects: {
-    type: Array,
-    required: false,
-  }
+  items: Array,
 });
 
+const createNew = () => {
+  router.visit(route('project.create'));
+};
 
+const edit = (id) => {
+  router.visit(route('project.edit', id));
+};
+
+const remove = (id) => {
+  if (confirm('Are you sure you want to delete this project?')) {
+    router.delete(route('project.destroy', id));
+  }
+};
 </script>
 
 <template>
-  <AppLayout>
-    <div class="p-4">
-      <h1 class="text-xl font-bold mb-4">Project List</h1>
-      <table class="w-full border-collapse border border-gray-300">
-        <thead>
-        <tr>
-          <th class='border border-gray-300 p-2'>Name</th>
-          <th class='border border-gray-300 p-2'>Description</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="project in projects" :key="project.id">
-          <td class='border border-gray-300 p-2'>{{ project.name }}</td>
-          <td class='border border-gray-300 p-2'>{{ project.description }}</td>
-        </tr>
-        </tbody>
-      </table>
-    </div>
-  </AppLayout>
+    <AppLayout>
+      <div class="p-4">
+        <h1 class="text-xl font-bold mb-4">Project List</h1>
+        <button @click="createNew" class="mb-4 p-2 bg-green-500 text-white rounded">Create New</button>
+        <table class="w-full border-collapse border border-gray-300">
+          <thead>
+          <tr>
+            <th class='border border-gray-300 p-2'>id</th>
+<th class='border border-gray-300 p-2'>name</th>
+<th class='border border-gray-300 p-2'>description</th>
+
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="item in items" :key="item.id">
+            <td class='border border-gray-300 p-2'>{{item.id}}</td>
+<td class='border border-gray-300 p-2'>{{item.name}}</td>
+<td class='border border-gray-300 p-2'>{{item.description}}</td>
+
+            <td class="border border-gray-300 p-2">
+              <button @click="edit(item.id)" class="mr-2 p-1 bg-yellow-500 text-white rounded">Edit</button>
+              <button @click="remove(item.id)" class="p-1 bg-red-500 text-white rounded">Delete</button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </AppLayout>
 </template>
